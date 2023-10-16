fda627de78be83d2d708068801d9cd36d9982dd8
Nicoly-Avelino <nicoly.avelino@fatec.sp.gov.br>
2023-10-16 11:18:43 -0300

    Adicionando o primeiro projeto feito em aula

---
+++ PrimeiroProjeto/app/src/main/java/com/example/primeiroprojeto/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.primeiroprojeto
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
